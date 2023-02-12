21f7e94713a5722b4c9be63bc9f830feacc7fa1e
Kamilprogrammer <86717843+kamilprogrammer@users.noreply.github.com>
2023-02-12 05:48:41 +0300

    adding lfask options

--- firebase/app.py
+++ firebase/app.py
@@ -1,14 +1,32 @@
-from flask import *
-import pyrebase4
-
+from  flask import Flask , session , render_template , flash
+import pyrebase
+app = Flask(__name__)
+#Firebase code:
+app.secret_key = 'kamIl044'
 config = {
     'apiKey': "AIzaSyB92b3L8ZzB_HUeeWgSEvq1SgpbF-paukc",
     'authDomain': "fajer-school.firebaseapp.com",
     'projectId': "fajer-school",
     'storageBucket': "fajer-school.appspot.com",
     'messagingSenderId': "935840445144",
-    'appId': "1:935840445144:web:f48027d09075a7fc7cddad"
+    'appId': "1:935840445144:web:f48027d09075a7fc7cddad",
+    'databaseURL': ""
 }
 
-fireabse = pyrebase4.initialize_app(config)
+
+fireabse = pyrebase.initialize_app(config)
 auth = fireabse.auth()
+email = 'kg0390217@gmail.com'
+password = 'kamIl044'
+
+user = auth.sign_in_with_email_and_password(email , password)
+print(user)
+
+auth.send_email_verification(user['idToken'])
+#to here firebase-end
+
+
+#The App routes:
+@app.route('/')
+def index():
+    render_template('index.html')
